ab6e2f6cb8594e153f01fafeccd0f71b27bb916b
Manuzau <138143069+Manuzau@users.noreply.github.com>
2024-06-08 11:26:18 +0100

    alteracao da tela paciente, e cadastro do paciente na bd

---
+++ src/utilitarios/TesteCon.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utilitarios;
+
+/**
+ *
+ * @author manue
+ */
+public class TesteCon {
+    public static void main(String[] args) {
+       //  Conectar c1 = new Conectar(); don´t need a instance
+       
+        System.out.println(Conectar.getConectar());
+       
+     }
+}
--- src/view/Home.java
+++ src/view/Home.java
@@ -2,10 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package virew;
+package view;
 
 import java.awt.Color;
-import virew.components.CadastrarPaciente;
+import view.components.CadastrarPaciente;
 
 /**
  *
