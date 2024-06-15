95ab420a8a26897477b7162f8fbbafd451b07165
Manuzau <138143069+Manuzau@users.noreply.github.com>
2024-06-15 16:31:07 +0100

    Foi criado a classe DAO para Consulta

--- src/dao/ConsultaDAO.java
+++ src/dao/ConsultaDAO.java
@@ -2,11 +2,40 @@ package dao;
 
 import java.sql.*;
 import javax.swing.JOptionPane;
+import models.Consulta;
+import controllers.Conectar;
 import java.sql.Connection;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.ArrayList;
 
 public class ConsultaDAO {
-      
+   
+   private static final Connection con = Conectar.getConectar();
+   
+   public static boolean cadastrarConsulta(Consulta c){
+       String sql = "INSERT INTO consulta (paciente_id,especialidade_id,medico_id,data_consulta,status)";
+       
+       try{
+           PreparedStatement smt = con.prepareStatement(sql);
+           smt.setString(1, c.getPaciente_id());
+           smt.setString(2, c.getEspecialidade());
+           smt.setString(3,c.getMedico_id());
+           smt.setString(4, c.getDataConsulta().toString());
+           smt.setBoolean(5,c.getStatus());
+           smt.executeUpdate();
+           JOptionPane.showMessageDialog(null,"Consulta agendada com sucesso");
+           smt.close();
+           con.close();
+       }catch(Exception ex){
+           JOptionPane.showMessageDialog(null,"Erro ao agendar consultar");
+       }
+       return true;
+   }
+   
+   public static boolean actualizarConsulta(Consulta c){
+       String sql = "UPDATE consulta SET ";
+       return  false;
+               //Por terminar...
+   }
 }
